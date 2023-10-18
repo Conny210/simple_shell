8e14a52f35eed52c72c2b1f6f7787318d8223fc4
Conny <conny.serite@yahoo.com>
2023-10-18 14:58:42 -0700

    added command interpreter

--- simple_shell.c
+++ simple_shell.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <string.h>
 
 #define MAX_COMMAND_LENGTH 100
@@ -8,25 +9,43 @@
  * main - start
  *
  * Return: 0
+ *
  */
 
 int main(void)
 {
 	char input[MAX_COMMAND_LENGTH];
-	int status = system(input);
 
 	while (1)
 	{
-		printf("$ ");
+		printf("#cisfun$ ");
 		if (fgets(input, sizeof(input), stdin) == NULL)
 		{
 			break;
 		}
+
 		input[strcspn(input, "\n")] = '\0';
 
-		if (status == -1)
+		if (strcmp(input, "exit") == 0)
 		{
-			printf("Error executing command\n");
+			break;
+		}
+		pid_t child_pid = fork();
+
+		if (child_pid == -1)
+		{
+			perror("fork");
+			exit(1);
+		}
+
+		if (child_pid == 0)
+		{
+			execlp(input, input, (char *)NULL);
+			perror(input);
+			exit(1);
+		} else
+		{
+			wait(NULL);
 		}
 	}
 	return (0);
